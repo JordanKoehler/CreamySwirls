0f26ea45f257210655e2e0aa572043aa916f2b6c
Jordan Koehler <jordan@Jordans-MacBook-Air.local>
2020-07-16 23:10:38 -0400

    fixed html into ejs with working nav bar and images

--- app.js
+++ app.js
@@ -11,8 +11,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(express.static("Assets"));
 
-app.get("/", function(req, res){
+app.get("/index", function(req, res){
   res.render("index");
+})
+
+app.get("/about", function(req, res){
+  res.render("about");
+})
+app.get("/FAQ", function(req, res){
+  res.render("FAQ");
+})
+
+app.get("/order", function(req, res){
+  res.render("order");
 })
 
 

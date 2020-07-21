e18ca6cb5d4ca1726cb812772756b949d14ce3e5
Jordan Koehler <jordan@Jordans-MacBook-Air.local>
2020-07-20 20:40:28 -0400

    confirm order now leads to yourOrder page. added jquery to order form

--- app.js
+++ app.js
@@ -5,26 +5,50 @@ const ejs = require("ejs");
 
 const app = express();
 
+const selectSize = [];
+const selectFlavours = [];
+const selectAdditions = [];
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 app.use(express.static("Assets"));
 
-app.get("/index", function(req, res){
+app.get("/", function(req, res) {
   res.render("index");
 })
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about");
 })
-app.get("/FAQ", function(req, res){
+app.get("/FAQ", function(req, res) {
   res.render("FAQ");
 })
 
-app.get("/order", function(req, res){
+app.get("/order", function(req, res) {
   res.render("order");
 })
+
+app.get("/logIn", function(req, res) {
+  res.render("login");
+})
+
+app.get("/thankYou", function(req, res) {
+  res.render("thankYou");
+})
+
+app.get("/yourOrder", function(req, res) {
+  res.render("yourOrder");
+})
+
+app.post("/order", function(req, res) {
+
+  res.redirect("yourOrder");
+})
+
 
 
 app.listen(3000, function() {
